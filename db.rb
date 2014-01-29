b178a398b4b2450cd08108a99af6efc664c483a1
Bryan Fordham <bfordham@gmail.com>
2014-01-29 17:34:51 -0500

    Modify db.rb for dev connections.
    Add seed.rb to refresh the data

--- db.rb
+++ db.rb
@@ -4,11 +4,14 @@ require 'uri'
 class Database
   def connect
   	return @db_connection if @db_connection
-	db = URI.parse(ENV['MONGOHQ_URL'])
-	db_name = db.path.gsub(/^\//, '')
-	@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-	@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-
+  	if ENV['MONGOHQ_URL']
+		db = URI.parse(ENV['MONGOHQ_URL'])
+		db_name = db.path.gsub(/^\//, '')
+		@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+		@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+	else
+		@db_connection = Mongo::Connection.new().db('drones')
+	end
 	@db_connection
   end
 end
