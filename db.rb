52a128b4cd8cd766f5ce2b6ce9862c4a45da0f51
Bryan Fordham <bfordham@gmail.com>
2014-01-29 16:58:11 -0500

    Setup Mongo connection on Heroku

--- db.rb
+++ db.rb
@@ -1,8 +1,14 @@
 require 'mongo'
+require 'uri'
 
 class Database
   def connect
-    db = Mongo::Connection.new.db("drones", :pool_size => 5, :timeout => 5)
-    return db
+  	return @db_connection if @db_connection
+	db = URI.parse(ENV['MONGOHQ_URL'])
+	db_name = db.path.gsub(/^\//, '')
+	@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+	@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+
+	@db_connection
   end
 end
